6b6d789efb270730044191c00b9c395eb563acb2
Huan <Huan.Wang2@dxc.com>
2020-05-19 14:49:14 +0800

    获取数据

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import io
 import sys
 import re
 import xlwt
-import xlrd
 import os
 import time
 import datetime
@@ -15,6 +14,7 @@ import json
 if __name__ == "__main__":
 	sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 	print("开始", flush = True)
+
 	# 文件名
 	excel_file_name = os.getcwd() + "\\result_" + \
 		datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".xls"
@@ -27,27 +27,39 @@ if __name__ == "__main__":
 	headers ={
 		"content-type":"application/json"
 	}
-	# 获取当前工会的所有年报
-	url_union = "https://www.cpppc.org:8082/api/pub/project/search"
-	param_union = {"created_date_order":"desc","dist_city":"","dist_code":"",
+	# 获取每页的项目id
+	proc_url = "https://www.cpppc.org:8082/api/pub/project/search"
+	proc_param = {"created_date_order":"desc","dist_city":"","dist_code":"",
 			"dist_province":"","end":"","industry":"",
 			"level":"","max":"10000000000000000","min":"0",
 			"name":"","pageNumber":"1","size":"5","start":"",
 			"status":["0","1","2"]}
-	r =requests.post(url_union, headers = headers, data = json.dumps(param_union))
+	proc_r =requests.post(proc_url, headers = headers, data = json.dumps(proc_param))
 
-	if r.status_code != 200:
-		raise Exception(r.status_code)
+	if proc_r.status_code != 200:
+		raise Exception(proc_r.status_code)
 
-	json_data = json.loads(r.text)
+	proc_json_data = json.loads(proc_r.text)
 
-	# 再次封装，获取具体标签内的内容
-	for ppp_data in json_data["data"]["hits"]:
-		print("工会编号:" + ppp_data["proj_rid"], flush = True)
-		print("--------------------------")
-		sheet.write(count,0, ppp_data["proj_rid"]) # row, column, value
-		workbook.save(excel_file_name)
-		count = count + 1;
-		time.sleep(2)
+	# 根据项目id，获取对应的数据
+	for proc_data in proc_json_data["data"]["hits"]:
+		pay_url = "https://www.cpppc.org:8082/api/pub/project/prepare-detail/" + proc_data["proj_rid"]
+		pay_r =requests.get(pay_url, headers = headers)
+		if pay_r.status_code != 200:
+			raise Exception(pay_r.status_code)
+
+		pay_json_data = json.loads(pay_r.text)
+		
+		for pay_data in pay_json_data["data"]["prepareFinancial"]["payDutyRatioList"]:
+			sheet.write(count,0, proc_data["proj_no"]) # row, column, value
+			sheet.write(count,1, pay_data["year"])
+			sheet.write(count,2, pay_data["ratioA"]/1000000)
+			sheet.write(count,3, pay_data["ratioA"]/1000000)
+			sheet.write(count,4, pay_data["ratioE"]/1000000)
+			sheet.write(count,5, pay_data["ratioG"]/1000000)
+			sheet.write(count,6, pay_data["ratio"])
+			workbook.save(excel_file_name)
+			count = count + 1;
+			time.sleep(2)
 
 	print("完成",flush = True)
