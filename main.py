e8ff16f97dc8005a1c9eeb8d50e7e18593449236
Huan <Huan.Wang2@dxc.com>
2020-05-19 13:56:08 +0800

    获取项目id

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import xlrd
 import os
 import time
 import datetime
+import json
 
 if __name__ == "__main__":
 	sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
@@ -23,14 +24,9 @@ if __name__ == "__main__":
 	workbook = xlwt.Workbook()
 	sheet = workbook.add_sheet("Sheet Name1")
 
-	# 获取cookie
-	url_cok = "https://www.cpppc.org:8082/inforpublic/homepage.html#/searchresult"
-	r_cok =requests.get(url_cok)
-	cookie_jar = r_cok.cookies
-
-	if r_cok.status_code != 200:
-		raise Exception(r_cok.status_code)
-
+	headers ={
+		"content-type":"application/json"
+	}
 	# 获取当前工会的所有年报
 	url_union = "https://www.cpppc.org:8082/api/pub/project/search"
 	param_union = {"created_date_order":"desc","dist_city":"","dist_code":"",
@@ -38,19 +34,20 @@ if __name__ == "__main__":
 			"level":"","max":"10000000000000000","min":"0",
 			"name":"","pageNumber":"1","size":"5","start":"",
 			"status":["0","1","2"]}
-	r =requests.post(url_union, param_union, cookies=cookie_jar)
+	r =requests.post(url_union, headers = headers, data = json.dumps(param_union))
 
 	if r.status_code != 200:
 		raise Exception(r.status_code)
 
+	json_data = json.loads(r.text)
+
 	# 再次封装，获取具体标签内的内容
-	for ppp_data in r.json():
-		print("工会编号:" + ppp_data["aaa"], flush = True)
-		print("年份:" + ppp_data["aaa"], flush = True)
-		print("内容:" + ppp_data["aaa"], flush = True)
+	for ppp_data in json_data["data"]["hits"]:
+		print("工会编号:" + ppp_data["proj_rid"], flush = True)
 		print("--------------------------")
-		sheet.write(count,0, ppp_data["aaa"]) # row, column, value
-		sheet.write(count,1, ppp_data["aaaa"])
-		sheet.write(count,2, ppp_data["aaaa"])
+		sheet.write(count,0, ppp_data["proj_rid"]) # row, column, value
+		workbook.save(excel_file_name)
+		count = count + 1;
+		time.sleep(2)
 
 	print("完成",flush = True)
